0e9b1782977ba1ff4caf7213a23a9b6718c77a45
V-Markovskis <vlads.markovskis@gmail.com>
2024-01-04 20:20:28 +0200

    posts page css

--- src/app/Components/PostsPage.tsx
+++ src/app/Components/PostsPage.tsx
@@ -9,35 +9,49 @@ import { Tag } from "@/app/types/tagType";
 export default function PostsPage({ posts }: { posts: Posts[] }) {
   return (
     <>
-      <div>
-        <Link href="/">
-          <button className="btn btn-success">Back to home page</button>
-        </Link>
-      </div>
       <div className={styles.globalContainer}>
-        {posts.map((post: Posts) => (
-          <div key={post.id} className={styles.innerContainer}>
-            <Image
-              src={post.image_url}
-              width={200}
-              height={200}
-              className={styles.image}
-              priority={false}
-              alt="post picture"
-            />
-            <Link href={`/posts/${post.id}`}>
-              <h3>{post.title}</h3>
-            </Link>
-            <div>
-              {post.tags.map((tag: Tag) => (
-                <div key={tag.id}>{tag.tag_name}</div>
-              ))}
+        <div>
+          <Link href="/">
+            <button className="btn btn-success">&larr; Back to Home</button>
+          </Link>
+        </div>
+        <div className={styles.postContainer}>
+          {posts.map((post: Posts) => (
+            <div key={post.id} className={styles.innerContainer}>
+              <div className={styles.imageTitleContainer}>
+                <Image
+                  src={post.image_url}
+                  width={200}
+                  height={200}
+                  className={styles.image}
+                  priority={false}
+                  alt="post picture"
+                />
+                <div
+                  className={styles.titleContainer}
+                  style={{ width: "200px", whiteSpace: "normal" }}
+                >
+                  <Link
+                    href={`/posts/${post.id}`}
+                    className="link-primary link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover"
+                  >
+                    <h3 className={styles.title}>{post.title}</h3>
+                  </Link>
+                </div>
+              </div>
+              <div className={styles.tagContainer}>
+                {post.tags.map((tag: Tag) => (
+                  <div key={tag.id} className={styles.tag}>
+                    {tag.tag_name}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        {posts.length === 0 && <p className="text-center">No posts created!</p>}
+        <br />
       </div>
-      {posts.length === 0 && <p className="text-center">No posts created!</p>}
-      <br />
     </>
   );
 }
