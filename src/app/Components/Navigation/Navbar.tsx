697edf55ba9fdab8edec72ffd00d2082b98dc01a
V-Markovskis <vlads.markovskis@gmail.com>
2024-01-04 19:11:03 +0200

    home page css

--- src/app/Components/Navigation/Navbar.tsx
+++ src/app/Components/Navigation/Navbar.tsx
@@ -2,33 +2,61 @@
 import Link from "next/link";
 import React from "react";
 import { useSession } from "next-auth/react";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { data: session } = useSession();
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      &nbsp;
-      <Link href={"/posts"}>Posts</Link>
-      {session?.user && (
-        <>
-          &nbsp;
-          <Link href={"/new-post"}>Add post</Link>
-        </>
-      )}
-      {session?.user ? (
-        <>
-          &nbsp;
-          {/*  https://next-auth.js.org/configuration/providers/oauth - contains links */}
-          <Link href={"/api/auth/signout"}>Logout</Link>
-        </>
-      ) : (
-        <>
-          &nbsp;
-          <Link href={"/api/auth/signin"}>Login</Link>
-        </>
-      )}
-    </nav>
+    <div>
+      <nav className={styles.container}>
+        <Link
+          href="/"
+          className="link-success link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover"
+        >
+          Home
+        </Link>
+        &nbsp;
+        <Link
+          href={"/posts"}
+          className="link-success link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover"
+        >
+          Posts
+        </Link>
+        {session?.user && (
+          <>
+            &nbsp;
+            <Link
+              href={"/new-post"}
+              className="link-success link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover"
+            >
+              Add post
+            </Link>
+          </>
+        )}
+        {session?.user ? (
+          <>
+            &nbsp;
+            {/*  https://next-auth.js.org/configuration/providers/oauth - contains links */}
+            <Link
+              href={"/api/auth/signout"}
+              className="link-success link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover"
+            >
+              Logout
+            </Link>
+          </>
+        ) : (
+          <>
+            &nbsp;
+            <Link
+              href={"/api/auth/signin"}
+              className="link-success link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover"
+            >
+              Login
+            </Link>
+          </>
+        )}
+      </nav>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,41 @@
 import Link from "next/link";
+import stylesHome from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <h2>Welcome to Our Blog</h2>
-        <p>
-          Explore a world of diverse and engaging content. Our blog covers a
-          wide range of topics, from thought-provoking articles to captivating
-          stories. Whether you're into technology, lifestyle, or creative arts,
-          there's something here for everyone.
-        </p>
-        <p>
-          Stay updated with the latest trends, discover unique perspectives, and
-          join the conversation. Our community is passionate about sharing
-          knowledge and experiences. Start your journey through the world of
-          ideas right here.
-        </p>
-        <p>
-          Ready to dive in? Browse our latest posts below and enjoy the richness
-          of content curated just for you.
-        </p>
+      <div className={stylesHome.container}>
+        <div className={stylesHome.header}>
+          <a href="https://git.io/typing-svg" className={stylesHome.image}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=500&size=30&pause=1000&background=3AFFF000&center=true&repeat=false&random=false&width=435&lines=Welcome+to+Our+Blog"
+              alt="Typing SVG"
+            />
+          </a>
+        </div>
+        <div className={stylesHome.innerContainer}>
+          <p>
+            Explore a world of diverse and engaging content. Our blog covers a
+            wide range of topics, from thought-provoking articles to captivating
+            stories. Whether you're into technology, lifestyle, or creative
+            arts, there's something here for everyone.
+          </p>
+          <p>
+            Stay updated with the latest trends, discover unique perspectives,
+            and join the conversation. Our community is passionate about sharing
+            knowledge and experiences. Start your journey through the world of
+            ideas right here.
+          </p>
+          <p>
+            Ready to dive in? Browse our latest posts below and enjoy the
+            richness of content curated just for you.
+          </p>
+        </div>
       </div>
 
-      <div>
-        <Link href="/posts">
+      <div className={stylesHome.buttonContainer}>
+        <Link href={"/posts"}>
           <button className="btn btn-success">View Posts</button>
         </Link>
       </div>
